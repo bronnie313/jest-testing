081d206d327c7836240abb1f4daf801f15a77f05
Bisaso Ronald <bronnie313@gmail.com>
2023-03-16 14:43:06 +0300

    commit task 3

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-const reverseString = (str) => {
-    return str.split("").reverse().join(""); 
+class Calculator {
+    add(a, b){
+        return a + b;
+    }
+    subtract(a, b){
+        return a-b;
+    }
+    divide(a, b){
+        return a / b;
+    }
+    multiply(a, b){
+        return a * b;
+    }
 }
 
-module.exports = reverseString;
-
+module.exports = Calculator;
 
 
--- main.test.js
+++ main.test.js
@@ -1,6 +1,71 @@
-const reverseString = require('./main');
+const Calculator = require('./main');
 
-test('reverse a given string', () => {
-    expect(reverseString('hello')).toBe('olleh');
-    expect(reverseString('hello, ronnie')).toBe('einnor ,olleh');
-});
+describe('Calculator',() => {
+
+    let calculator;
+
+    beforeEach(() => {
+        calculator = new Calculator();
+    });
+
+    test('calculate addition', () => {
+        expect(calculator.add(2, 3)).toBe(5);
+    })
+    test('add to numbers', () => {
+        expect(calculator.add(5, 5)).toBe(10);
+    })
+})
+
+describe('Calculator', () => {
+    let calculator;
+
+    beforeEach(() => {
+        calculator = new Calculator();
+    })
+
+    test('subtract two numbers', () => {
+        expect(calculator.subtract(5, 5)).toBe(0);
+    })
+    test('subtract two numbers', () => {
+        expect(calculator.subtract(6, 2)).toBe(4);
+    })
+    test('subtract two numbers', () => {
+        expect(calculator.subtract(6, 3)).toBe(3);
+    })
+})
+
+describe('Calculator', () => {
+    let calculator;
+
+    beforeEach(() => {
+        calculator = new Calculator();
+    })
+
+    test('divide two numbers', () => {
+        expect(calculator.divide(10, 2)).toBe(5);
+    })
+    test('divide two numbers', () => {
+        expect(calculator.divide(10, 5)).toBe(2);
+    })
+    test('divide two numbers', () => {
+        expect(calculator.divide(10, 10)).toBe(1);
+    })
+})
+
+describe('Calculator', () => {
+    let calculator;
+
+    beforeEach(() => {
+        calculator = new Calculator();
+    })
+
+    test('multiply two numbers', () => {
+        expect(calculator.multiply(10, 2)).toBe(20);
+    })
+    test('multiply two numbers', () => {
+        expect(calculator.multiply(10, 5)).toBe(50);
+    })
+    test('multiply two numbers', () => {
+        expect(calculator.multiply(10, 10)).toBe(100);
+    })
+})
